c41b5880787168e55f7c847e88ae258d0f19d84b
Vidya <118951865+Vidz2311@users.noreply.github.com>
2023-04-13 09:14:03 +0000

    we did a program sucessfully!!!!!!!

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello LOLS BACCHA & Keer")
+
+name=input("Enter your name:")
+time=int(input("Enter the current time:"))
+if(time<=12):
+ print("Good Morning")
+elif(time<=16):
+ print("Good Afternoon")
+elif(time<=20):
+ print("Good Evening")
+else:
+ print("Good Night")
